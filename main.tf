fdeaef82a396815a131a0b1e8fb79811b4b36e65
Arun2602 <100331455+Arun2602@users.noreply.github.com>
2022-06-22 16:35:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      
+      version = "~> 3.0"
     }
   }
 }
@@ -12,8 +12,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA5ITFTZRUA43SI5JL"
-  secret_key = "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"
+  access_key = "AKIA5JKCP6HX5LPPXA6P"
+  secret_key = "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"
 }
 
 /* ==================== VPC ============================= */
@@ -22,7 +22,7 @@ provider "aws" {
 resource "aws_vpc" "my-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Terraform-VPC"
+    Name = "Test-VPC"
   }
 }
 
@@ -34,7 +34,7 @@ resource "aws_subnet" "web-subnet-1" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Websubter-1a"
+    Name = "Websub-1a"
   }
 }
 
@@ -45,7 +45,7 @@ resource "aws_subnet" "web-subnet-2" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "Websubter-2b"
+    Name = "Websub-2b"
   }
 }
 
@@ -57,7 +57,7 @@ resource "aws_subnet" "application-subnet-1" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "Applicationsubter-1a"
+    Name = "Applicationsub-1a"
   }
 }
 
@@ -68,7 +68,7 @@ resource "aws_subnet" "application-subnet-2" {
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "Applicationsubter-2b"
+    Name = "Applicationsub-2b"
   }
 }
 
@@ -79,7 +79,7 @@ resource "aws_subnet" "database-subnet-1" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "Databasesubter-1a"
+    Name = "Databasesub-1a"
   }
 }
 
@@ -89,7 +89,7 @@ resource "aws_subnet" "database-subnet-2" {
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "Databasesubter-2b"
+    Name = "Databasesub-2b"
   }
 }
 
@@ -100,7 +100,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.my-vpc.id
 
   tags = {
-    Name = "Testter-IGW"
+    Name = "Test-IGW"
   }
 }
 
@@ -115,7 +115,7 @@ resource "aws_route_table" "web-rt" {
   }
 
   tags = {
-    Name = "Webter-RT"
+    Name = "Web-RT"
   }
 }
 
@@ -142,7 +142,7 @@ resource "aws_instance" "webserver1" {
   user_data              = file("install_apache.sh")
 
   tags = {
-    Name = "Web Server ter 1"
+    Name = "Web Server 1"
   }
 
 }
@@ -156,7 +156,7 @@ resource "aws_instance" "webserver2" {
   user_data              = file("install_apache.sh")
 
   tags = {
-    Name = "Web Server ter 2"
+    Name = "Web Server 2"
   }
 
 }
@@ -185,7 +185,7 @@ resource "aws_security_group" "web-sg" {
   }
 
   tags = {
-    Name = "Webter-SG"
+    Name = "Web-SG"
   }
 }
 
@@ -211,7 +211,7 @@ resource "aws_security_group" "webserver-sg" {
   }
 
   tags = {
-    Name = "Webserverter-SG"
+    Name = "Webserver-SG"
   }
 }
 
@@ -237,14 +237,14 @@ resource "aws_security_group" "database-sg" {
   }
 
   tags = {
-    Name = "Databaseter-SG"
+    Name = "Database-SG"
   }
 }
 
 /* =========================== ELB ===============================*/
 
 resource "aws_lb" "external-elb" {
-  name               = "Externalter-LB"
+  name               = "External-LB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web-sg.id]
@@ -310,7 +310,7 @@ resource "aws_db_subnet_group" "default" {
   subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
 
   tags = {
-    Name = "My DB subnet group ter"
+    Name = "My DB subnet group"
   }
 }
 
